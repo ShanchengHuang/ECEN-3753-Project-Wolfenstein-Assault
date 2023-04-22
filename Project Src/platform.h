cb6ebad2323e63a4a224fcd0f6b001f86d980d54
JackHuang <shancheng.huang@colorado.edu>
2023-04-22 16:58:20 -0600

    updates

---
+++ Project Src/platform.h
@@ -0,0 +1,29 @@
+
+#ifndef PLATFORM_H_
+#define PLATFORM_H_
+
+#include "stdbool.h"
+#include "constants.h"
+#include "os.h"
+
+//all in pixels
+#define PLATFORM_Y SCREEN_PIXELS- 15
+#define PLATFORM_HEIGHT 5
+#define PLATFORM_WIDTH 30
+
+#define PLATFORM_MASS 10 //kg
+#define MAX_PIXEL_FORCE 100 //kg * px/s^2
+#define MAX_FORCE MAX_PIXEL_FORCE * SCREEN_MM / SCREEN_PIXELS //kg * px/s^s
+#define MAX_PIXEL_ACCEL MAX_PIXEL_FORCE / PLATFORM_MASS
+
+struct PlatformData {
+  double x;
+  double vx;
+  double ax;
+};
+
+void platform_task_create(void);
+void platform_task(void);
+void platform_timer_cb(void);
+
+#endif /* PLATFORM_H_ */
