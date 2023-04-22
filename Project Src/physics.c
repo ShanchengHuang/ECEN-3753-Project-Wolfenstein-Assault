8db686d4c40b1098b7ffc31206b65cf454011fde
ShanchengHuang <Shancheng.Huang@colorado.edu>
2023-04-22 15:44:33 -0600

    update

--- Project Src/physics.c
+++ Project Src/physics.c
@@ -95,71 +95,33 @@ void updatePlatformForce()
 	applyForceToPlatform(force);
 }
 
+void update_platform(SharedData * shared_data) {
+  shared_data->xPos += shared_data->xVel * PHYSICS_DELTA;
+  shared_data->xVel += shared_data->capSenseForce * PHYSICS_DELTA;
 
-void railgun_task_create(void) {
-  RTOS_ERR semErr;
-  RTOS_ERR tskErr;
-  OSSemCreate(
-      &railgun_semaphore,
-      "railgun semaphore",
-      0,
-      &semErr
-  );
-  OSTaskCreate(
-         &railgunTCB,                /* Pointer to the task's TCB.  */
-        "Railgun Task.",                    /* Name to help debugging.     */
-        &railgun_task,                   /* Pointer to the task's code. */
-         DEF_NULL,                          /* Pointer to task's argument. */
-         NORMAL_PRIORITY,             /* Task's priority.            */
-        &railgunSTK[0],             /* Pointer to base of stack.   */
-        (STACK_SIZES / 10u),  /* Stack limit, from base.     */
-         STACK_SIZES,         /* Stack size, in CPU_STK.     */
-         10u,                               /* Messages in task queue.     */
-         120u,                                /* Round-Robin time quanta.    */
-         DEF_NULL,                          /* External TCB data.          */
-         OS_OPT_TASK_STK_CHK,               /* Task options.               */
-        &tskErr
-      );
-  if (semErr.Code || tskErr.Code) EFM_ASSERT(false);
-}
+  float half_platform_length = default_config.platform.length / 2;
 
-bool shoot_railgun(int idx) {
-  if (railgun_shots > 0) {
-    railgun_shots--;
-    score++;
-    railgun_fired = 3;
-    shotX = Platforms[idx].x;
-    shotY = Platforms[idx].y;
-    RTOS_ERR mutexErr;
-    OSMutexPend(&platform_mutex, 0, OS_OPT_PEND_BLOCKING, NULL, &mutexErr);
-    if (mutexErr.Code) EFM_ASSERT(false);
-
-    generate_platform(idx);
-
-    OSMutexPost(&platform_mutex, OS_OPT_POST_NONE, &mutexErr);
-    if (mutexErr.Code) EFM_ASSERT(false);
-    return true;
-  }
-  return false;
-}
-
-void railgun_task(void) {
-  RTOS_ERR semErr;
-  RTOS_ERR mutexErr;
-  while (1) {
-      OSSemPend(&railgun_semaphore, 0, OS_OPT_PEND_BLOCKING, NULL, &semErr);
-      if (semErr.Code) EFM_ASSERT(false);
-      //TODO check railgun shot count, if good, decrement and pend platform mutex and respawn it
-      int lowestIdx = 0;
-      int lowestHeight = 0; //lowest is highest y
-      for (int i = 0; i < PLATFORM_COUNT; i++) {
-          if (lowestHeight < Platforms[i].y) {
-              lowestHeight = Platforms[i].y;
-              lowestIdx = i;
-          }
+  if ((shared_data->xPos - half_platform_length) < CANYON_START) {
+      if (shared_data->xVel < (-1 * default_config.platform.max_platform_bounce_speed)) {
+          game_over("Too Fast");
+      } else if (PLATFORM_BOUNCE_ENABLED) {
+          shared_data->capSenseForce = 0;
+          shared_data->xVel = fabs(shared_data->xVel);
+      } else {
+          shared_data->capSenseForce = 0;
+          shared_data->xVel = 0;
+          shared_data->xPos = CANYON_START + half_platform_length;
       }
-      shoot_railgun(lowestIdx);
+  } else if ((shared_data->xPos + half_platform_length) > CANYON_END) {
+      if (shared_data->xVel > default_config.platform.max_platform_bounce_speed) {
+          game_over("Too Fast");
+      } else if (PLATFORM_BOUNCE_ENABLED) {
+          shared_data->capSenseForce = 0;
+          shared_data->xVel = -1 * fabs(shared_data->xVel);
+      } else {
+          shared_data->capSenseForce = 0;
+          shared_data->xVel = 0;
+          shared_data->xPos = CANYON_END - half_platform_length;
+      }
   }
 }
-
-// ... other code ...
